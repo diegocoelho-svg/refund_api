f1e8dc82a405743bbcba20aa3798156d5024b34e
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-09-01 23:23:20 -0300

    feat(auth): verify user existence and password in session login

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express'
+import { prisma } from '@/database/prisma'
+import { AppError } from '@/utils/AppError'
+import { compare } from 'bcrypt'
 import { z } from 'zod'
 
 class SessionsController {
@@ -9,6 +12,18 @@ class SessionsController {
     })
 
     const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email } })
+
+    if (!user) {
+      throw new AppError("E-mail ou senha inválido", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail ou senha inválido", 401)
+    }
 
     response.json({ email, password })
   }
