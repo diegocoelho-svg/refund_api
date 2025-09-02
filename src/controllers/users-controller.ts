0329323e0308153a8e64e8b11e0cd2c7392bed52
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-09-01 22:45:09 -0300

    feat(user): create user with password encryption using bcrypt

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { UserRole } from '@prisma/client'
+import { prisma } from '@/database/prisma'
 import { z } from 'zod'
+import { hash } from 'bcrypt'
+import { AppError } from '@/utils/AppError'
 
 class UsersController {
   async create(request: Request, response: Response) {
@@ -19,7 +22,24 @@ class UsersController {
 
     const { name, email, password, role } = bodySchema.parse(request.body)
 
-    response.json({ name, email, password, role })
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new AppError("Já existe um usuário cadastrado com esse e-mail")
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      }
+    })
+
+    response.status(201).json()
   }
 }
 
