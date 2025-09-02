db249b8fba7b1cd90a35438456614b1968604134
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-09-01 23:42:03 -0300

    feat(refunds): add controller and routes for refunds

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,16 @@ import { Router } from 'express'
 
 import { usersRoutes } from './users-routes'
 import { sessionsRoutes } from './sessions-routes'
+import { refundsRoutes } from './refunds-routes'
 
 const routes = Router()
 
+// Public Routes
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
+
+// Private Routes
+routes.use("/refunds", refundsRoutes)
+
 
 export { routes }
